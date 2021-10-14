30d92ee765149a6108002d748ca9ae9a652c309c
Sergey Aristov <aristov775@gmail.com>
2021-10-14 12:19:29 +0300

    Complete done tasks: DistinctUntilChanged

--- src/main/kotlin/8_DistinctUntilChanged.kt
+++ src/main/kotlin/8_DistinctUntilChanged.kt
@@ -1,8 +1,11 @@
 import io.reactivex.rxjava3.core.Observable
 
-
 fun main() {
     Observable.fromArray(1, 2, 3, 4, 4, 5, 5, 6, 77, 6, 5, 4, 7, 3, 2, 1)
-        .distinct()
+        .also {observable ->
+            observable.subscribe { print(" $it") }
+        }
+        .doOnSubscribe { println() }
+        .distinctUntilChanged()
         .subscribe { print(" $it") }
 }
